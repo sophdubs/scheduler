0567ae3af24f14aa67eb2babc6ff43dbb6a5c249
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-07-07 19:39:19 -0400

    Created Form component and stories

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+export default function Form(props) {
+  const [name, setName] = useState(props.name || null);
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  function reset() {
+    setName("");
+    setInterviewer(null);
+    props.onCancel();
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={e => e.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            value={name}
+            type="text"
+            placeholder="Enter Student Name"
+            onChange={e => setName(e.target.value)}
+            /*
+              This must be a controlled component
+            */
+          />
+        </form>
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={reset}>Cancel</Button>
+          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
