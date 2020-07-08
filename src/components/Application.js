fa3f1ef52770d49d8f93fbc1342d6bee0b5a5855
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-07-08 17:18:47 -0400

    Using promise.all to resolve both axios calls at same time and update state all at once

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,69 +5,31 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
 
-
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Archie Cohen",
-      interviewer: { 
-        id: 2, 
-        name: "Tori Malcolm", 
-        avatar: "https://i.imgur.com/Nmx0Qxo.png" }
-    }
-  },
-  {
-    id: 4,
-    time: "3pm"
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Maria Boucher",
-      interviewer: { 
-        id: 5, 
-        name: "Sven Jones", 
-        avatar: "https://i.imgur.com/twYrpay.jpg" }
-    }
-  },
-  {
-    id: 6,
-    time: "5pm"
-  }
-];
+import { getAppointmentsForDay } from "../helpers/selectors";
 
 
 export default function Application(props) {
-  let [selectedDay, updateSelectedDay] = useState('Monday');
-  let [days, setDays] = useState([]);
+  
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
   useEffect(() => {
-    axios.get('/api/days')
-    .then(response => {
-      
-      setDays(response.data);
-    })
+    const fetchDays = axios.get('/api/days');
+    const fetchAppointments = axios.get('/api/appointments');
+    Promise.all([fetchDays, fetchAppointments])
+      .then(([daysRes, aptRes]) => {
+        setState(prev => ({...prev, days: daysRes.data, appointments: aptRes.data}));
+      })
   }, []);
+
+  const setDay = day => {
+    setState({...state, day});
+  }
+
+  const appointments = getAppointmentsForDay(state, state.day);
 
   return (
     <main className="layout">
@@ -80,9 +42,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={selectedDay}
-            setDay={updateSelectedDay}
+            days={state.days}
+            day={state.day}
+            setDay={setDay}
           />
         </nav>
         <img
