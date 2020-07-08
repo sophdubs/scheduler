81fca0d843f65c34a84ee36fa835caa00b54782e
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-07-08 15:47:22 -0400

    Use useEffect and axios to fetch the days from our api and render them in our component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 
 const appointments = [
   {
@@ -74,6 +59,16 @@ const appointments = [
 
 export default function Application(props) {
   let [selectedDay, updateSelectedDay] = useState('Monday');
+  let [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days')
+    .then(response => {
+      
+      setDays(response.data);
+    })
+  }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
