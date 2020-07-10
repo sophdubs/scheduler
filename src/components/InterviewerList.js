f0dad629df3bc1fa845e10e52e46526918e5c423
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-07-10 15:59:59 -0400

    Import forgotten stylesheet

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import InterviewerListItem from "components/InterviewerListItem";
+import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
   return (
