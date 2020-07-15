85cc38494b5769a41f1e19cccba445f7908f6079
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-07-15 16:59:04 -0400

    Added last of our E2E testing by writing cypress tests for editing interview and deleting interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -14,12 +14,46 @@ describe("Appointments", () => {
       .type("Lydia Miller-Jones");
 
     cy.get("[alt='Sylvia Palmer']")
-      .click()
+      .click();
 
     cy.contains("Save")
       .click(); 
       
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it ("should edit and interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({force: true});
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Tori Malcolm']")
+      .click();
+
+    cy.contains("Save")
+      .click(); 
+    
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]")
+      .first()
+      .click({force: true});
+
+    cy.contains("Confirm")
+      .click();
+
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones")
+      .should("not.exist");
   });
 });
